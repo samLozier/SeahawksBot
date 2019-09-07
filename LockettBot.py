221c3d41460c00f309b1d0869c81d602d0335e1a
samthaman <7992371+samthaman@users.noreply.github.com>
2019-09-07 16:27:13 -0700

    Creating the LocketteBot to prompt people to spell the name correctly.

---
+++ LockettBot.py
@@ -0,0 +1,54 @@
+import praw
+import time
+import creds
+
+reddit = praw.Reddit(client_id=creds['client_id'],
+                     client_secret=creds['client_secret'],
+                     password=creds['password'],
+                     user_agent=creds['user_agent'],
+                     username=creds['username'])
+
+
+def search():
+    prev_ids = open('commentid.txt', "a+")
+    reply_log = open("reply_log.txt", "w+")
+    for results in reddit.subreddit(
+            'Seahawks').comments():  # Grab all the Recent Comments in every subreddit. This will return 100 of the newest comments on Reddit
+        body = results.body.lower()   # Grab the Comment
+        comment_id = results.id  # Get the Comment ID
+        author = results.author
+        if comment_id in prev_ids:  # Check if we already replied to this comment
+            continue
+        else:
+            found = str(body.find('lockette'))
+            ricardo = str(body.find('ricardo'))
+            print(body)
+            if found != '-1' and ricardo == '-1' and author != 'LocketteBot':  # Looks like the comment references the wrong player
+                try:
+                    results.reply("I'm the Lockett(e) bot. You typed Lockett**e** but you might have meant to type **Lockett** (no 'e')\n\n"
+                                  "**[Ricardo Lockette](https://en.wikipedia.org/wiki/Ricardo_Lockette)** was on the superbowl winning team but last played in 2015 after suffering a neck injury\n\n"
+                                  "**[Tyler Lockett](https://en.wikipedia.org/wiki/Tyler_Lockett)** Is the current #1 Receiver on the team, he also spells his name differently."
+                                  )
+                    replied_to = {
+                        'comment_id': comment_id,
+                        'author': author,
+                        'body': body
+                    }
+                    reply_log.write(replied_to)
+                    prev_ids.write(comment_id)
+                except:
+                    continue  # Leave the function if error occurs with replying
+
+    prev_ids.close()
+    reply_log.close()
+
+
+
+
+
+
+
+while True:
+    search()
+    time.sleep(5)
+
